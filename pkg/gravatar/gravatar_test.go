5cc99bf362919cb5be3c9572076086aebf1bbc5d
judegiordano <judegiordano@gmail.com>
2025-01-21 19:45:51 -0500

    make hash function public

--- pkg/gravatar/gravatar_test.go
+++ pkg/gravatar/gravatar_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestHash(t *testing.T) {
 	email := "mail@mail.com"
-	hash := generateHash(email)
+	hash := GenerateHash(email)
 	assert.Equal(t, hash, "7905d373cfab2e0fda04b9e7acc8c879")
 	url := Url(email)
 	assert.Equal(t, url, fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=retro", hash))
