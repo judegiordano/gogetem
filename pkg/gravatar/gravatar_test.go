de2c7a04a0234fe711ca01a6d7d6e3985fd3fbd3
judegiordano <judegiordano@gmail.com>
2025-01-31 21:37:03 -0500

    gravatar struct

--- pkg/gravatar/gravatar_test.go
+++ pkg/gravatar/gravatar_test.go
@@ -2,6 +2,8 @@ package gravatar
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +13,67 @@ func TestHash(t *testing.T) {
 	email := "mail@mail.com"
 	hash := GenerateHash(email)
 	assert.Equal(t, hash, "7905d373cfab2e0fda04b9e7acc8c879")
-	url := Url(email)
-	assert.Equal(t, url, fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=retro", hash))
+	url := Url(email, "2048")
+	assert.Equal(t, url, fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=retro&s=2048", hash))
+}
+
+func TestRequest(t *testing.T) {
+	email := "mail@mail.com"
+	bytes, err := Image(email, 2048)
+
+	// should build bytes
+	assert.Nil(t, err)
+	assert.True(t, len(bytes) >= 1)
+
+	path := os.TempDir()
+	path = fmt.Sprint(filepath.Join(path, "avatar.png"))
+
+	// should be able to save as img
+	err = os.WriteFile(path, bytes, 0644)
+	assert.Nil(t, err)
+
+	// img should exist
+	img, err := os.ReadFile(path)
+	assert.Nil(t, err)
+	assert.True(t, len(img) >= 1)
+
+	// should remove path
+	err = os.Remove(path)
+	assert.Nil(t, err)
+
+	// path should not exist
+	missing, err := os.ReadFile(path)
+	assert.NotNil(t, err)
+	assert.Nil(t, missing)
+}
+
+func TestGenerate(t *testing.T) {
+	gravatar := Gravatar{
+		Email:   "mail@mail.com",
+		Size:    2048,
+		Default: Identicon,
+	}
+	data, err := gravatar.Generate()
+	assert.NotNil(t, data)
+	assert.Nil(t, err)
+
+	path := fmt.Sprint(filepath.Join("./", "avatar.png"))
+
+	// should be able to save as img
+	err = os.WriteFile(path, data.Image, 0644)
+	assert.Nil(t, err)
+
+	// img should exist
+	img, err := os.ReadFile(path)
+	assert.Nil(t, err)
+	assert.True(t, len(img) >= 1)
+
+	// should remove path
+	err = os.Remove(path)
+	assert.Nil(t, err)
+
+	// path should not exist
+	missing, err := os.ReadFile(path)
+	assert.NotNil(t, err)
+	assert.Nil(t, missing)
 }
--- pkg/gravatar/main.go
+++ pkg/gravatar/main.go
@@ -4,8 +4,25 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/judegiordano/gogetem/pkg/logger"
 )
+
+type Gravatar struct {
+	Email   string
+	Size    int
+	Default Default
+}
+
+type GravatarData struct {
+	EmailHash string
+	Url       string
+	Image     []byte
+}
 
 func GenerateHash(email string) string {
 	norm := strings.ToLower(strings.TrimSpace(email))
@@ -13,7 +30,61 @@ func GenerateHash(email string) string {
 	return hex.EncodeToString(hash[:])
 }
 
-func Url(email string) string {
+func Url(email string, size string) string {
 	hash := GenerateHash(email)
-	return fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=retro", hash)
+	return fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=retro&s=%v", hash, size)
 }
+
+func Image(email string, size int) ([]byte, error) {
+	s := strconv.Itoa(size)
+	url := Url(email, s)
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Error("GRAVATAR REQUEST", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("GRAVATAR BYTE READ", err)
+		return nil, err
+	}
+	return bytes, nil
+}
+
+func (g *Gravatar) Generate() (*GravatarData, error) {
+	size := strconv.Itoa(g.Size)
+	hash := GenerateHash(g.Email)
+	var d string
+	if len(g.Default) == 0 {
+		d = string(Retro)
+	} else {
+		d = string(g.Default)
+	}
+
+	url := fmt.Sprintf("https://www.gravatar.com/avatar/%v?d=%v&s=%v", hash, d, size)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		logger.Error("GRAVATAR REQUEST", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("GRAVATAR BYTE READ", err)
+		return nil, err
+	}
+	return &GravatarData{EmailHash: hash, Url: url, Image: bytes}, nil
+}
+
+type Default string
+
+const (
+	MysteryPerson Default = "mp"
+	Identicon     Default = "identicon"
+	Monsterid     Default = "monsterid"
+	Wavatar       Default = "wavatar"
+	Retro         Default = "retro"
+	Robohash      Default = "robohash"
+)
